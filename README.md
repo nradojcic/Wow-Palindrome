a5874fa17955492bdf72e36b3fcdf181548aeeb2
nradojcic <63752654+nradojcic@users.noreply.github.com>
2021-07-27 00:28:29 -0400

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# JS-Site-Template
-Site Template for JS Projects
+# Wow! Palindrome
+
+- A Palindrome is a word, phrase, or sequence that reads the same forwards and backwards.
+- For example - Wow, Racecar, Kayak, Radar, Taco Cat, Madam I'm Adam, etc.
+- Write a program that checks if the phrase entered is a Palindrome.
+
+### It demonstrates:
+
+- CSS and Bootstrap Responsive Layout
+- JavaScript Fundamentals
+- JavaScript Loops
+- JavaScript Functions
+- JavaScript DOM Manipulation
+- JavaScript If/Then/Else
+- JavaScript Boolean Logic
--- js/site.js
+++ js/site.js
@@ -1,4 +1,64 @@
-// Call Hello World
-function HelloWorld() {
-  alert("Hello World");
+// get user input
+// Controller function
+function getValue() {
+  // make sure the alert box is off and not styled at the beginning
+  document.getElementById("alert").classList.add("invisible");
+  document
+    .getElementById("alert")
+    .classList.remove("alert-danger", "alert-success");
+
+  // get user string from the page
+  let userString = document.getElementById("userString").value;
+
+  // check for a palindrome
+  let returnObj = checkForPalindrome(userString);
+
+  // display a message to the webpage
+  displayMessage(returnObj);
+}
+
+// check if string is a palindrome
+// Logic function
+function checkForPalindrome(userString) {
+  // convert to lowercase, and then ...
+  userString = userString.toLowerCase();
+
+  // ... remove any spaces and special characters before checking
+  let regex = /[^a-z0-9]/gi;
+  userString = userString.replace(regex, "");
+
+  // reverse the string
+  let revString = [];
+  let returnObj = {};
+
+  for (let i = userString.length - 1; i >= 0; i--) {
+    revString += userString[i];
+  }
+
+  // check if both strings are the same
+  if (revString == userString) {
+    returnObj.msg = "Well  done! Yes! You entered a Palindrome!";
+    document.getElementById("alert").classList.add("alert-success");
+  } else {
+    returnObj.msg = "Sorry! No, it's NOT a Palindrome!";
+    document.getElementById("alert").classList.add("alert-danger");
+  }
+
+  returnObj.reversed = revString;
+
+  // return the object
+  return returnObj;
+}
+
+// display a message on the webpage
+// View function
+function displayMessage(returnObj) {
+  // write heading and the message
+  document.getElementById("alert-heading").innerHTML = returnObj.msg;
+  document.getElementById(
+    "msg"
+  ).innerHTML = `Your Phrase reversed is: <strong>${returnObj.reversed}</strong>`;
+
+  // turn the alert on
+  document.getElementById("alert").classList.remove("invisible");
 }
